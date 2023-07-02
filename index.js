4904ee23387e908686561587a54c2a18b2ef42e1
Daniel Kamen <dsk3110@yahoo.com>
2023-07-02 12:12:37 -0400

    added square

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ const c = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
-c.fillStyle = 'red';
+c.fillStyle = 'white';
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+c.fillStyle = 'red';
+c.fillRect(200, 100, 100, 100);
